2e232ea8dfb7c51b7409084fb91a03461b7227e6
Michele Berretti <michele.berretti@unitn.it>
2026-01-15 14:14:01 +0100

    Hotfix

--- soft/__init__.py
+++ soft/__init__.py
@@ -6,9 +6,6 @@ import skimage
 import pandas
 import os
 import glob
-from numba import jit
-import matplotlib.pyplot
-import matplotlib.animation
 import tqdm 
 import multiprocessing
 import time
--- soft/extras.py
+++ soft/extras.py
@@ -5,9 +5,6 @@ import skimage
 import pandas
 import os
 import glob
-from numba import jit
-import matplotlib.pyplot
-import matplotlib.animation
 import tqdm 
 import multiprocessing
 import time
