b2f2081a4b0e726b187e89a46040e0651571e8ff
Michele Berretti <150249871+mib-unitn@users.noreply.github.com>
2026-02-24 12:30:53 +0100

    Fix missing requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='solar_ft',
-    version='2.2.1',
+    version='2.5.1',
     description='A Python lib for Solar Feature Tracking',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -25,6 +25,8 @@ setup(
                       'tqdm',
                       'pathos',
                       'typing',
+                      'EMD-signal',
+                      'sunpy[all]'
                       ],
 
     classifiers=[
