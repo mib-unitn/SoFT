6f916a8f4f6cec2513912bb967c1e5360080001d
Michele Berretti <150249871+mib-unitn@users.noreply.github.com>
2026-07-23 14:48:27 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='solar_ft',
-    version='3.0.1',
+    version='3.0.2',
     description='A Python lib for Solar Feature Tracking',
     long_description=long_description,
     long_description_content_type='text/markdown',
